692e754762a981340a24ee79eae4b79fbafeb6a6
tonejman <nej.man@interia.pl>
2023-07-17 19:39:59 +0200

    created components and  code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,130 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Searchbar from './Searchbar';
+import ImageGallery from './ImageGallery';
+import Button from './Button';
+import getParams, { getTotalPages } from './apiget';
+import { Loader } from './Loader';
+import Modal from './Modal';
+
+export class App extends Component {
+  state = {
+    images: [],
+    inputSearch: '',
+    currentPage: 1,
+    totalPages: 0,
+    isLoading: false,
+    setLoading: false,
+    showModal: false,
+    largeImageURL: '',
+  };
+
+  handleSubmit = event => {
+    this.setState({ inputSearch: event, currentPage: 1, isLoading: true });
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    try {
+      const { inputSearch, currentPage, totalPages } = this.state;
+
+      if (
+        prevState.inputSearch !== inputSearch ||
+        prevState.currentPage !== currentPage
+      ) {
+        const data = await getParams({
+          inputSearch,
+          currentPage,
+        });
+
+        if (prevState.inputSearch !== inputSearch) {
+          this.setState({
+            images: data.hits,
+            totalPages: getTotalPages(data.totalHits, 12),
+          });
+
+          return this.setState({ isLoading: false });
+        }
+
+        if (totalPages < currentPage) {
+          this.setState({ currentPage: totalPages + 1 });
+        }
+        this.setState(prevState => ({
+          images: [...prevState.images, ...data.hits],
+        }));
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  handleLoadMore = () => {
+    this.setState(prevState => ({
+      currentPage: prevState.currentPage + 1,
+    }));
+  };
+
+  ShowButton = () => {
+    const { images, inputSearch, currentPage, totalPages } = this.state;
+    const noMoreImages = totalPages === currentPage;
+
+    if (images.length < 12 || noMoreImages) {
+      return false;
+    } else if (inputSearch !== null) {
+      return true;
+    }
+  };
+
+  onClickImage = url => {
+    this.setState({ showModal: true, largeImageURL: url });
+  };
+  onModalClose = () => {
+    this.setState({ showModal: false, largeImageURL: '' });
+  };
+
+  onLoader = () => {
+    this.setState({ isLoading: false });
+  };
+
+  handleLoader = () => {
+    const { isLoading, setLoading } = this.state;
+    if (!isLoading) {
+      setLoading(true);
+    } else {
+      setTimeout(() => {
+        setLoading(false);
+      }, 3500);
+    }
+  };
+
+  render() {
+    const { images, isLoading, showModal, largeImageURL } = this.state;
+
+    return (
+      <div className="wrapper">
+        <Searchbar onSubmit={this.handleSubmit} />
+        {isLoading && <Loader name="ThreeDots" />}
+        <ImageGallery
+          images={images}
+          onLoadMore={this.handleLoadMore}
+          onClickImage={this.onClickImage}
+        />
+        {this.ShowButton() && (
+          <Button
+            onLoadMore={this.handleLoadMore}
+            onClick={this.handleLoader}
+          />
+        )}
+
+        {showModal && (
+          <Modal
+            largeImageURL={largeImageURL}
+            onModalClose={this.onModalClose}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
